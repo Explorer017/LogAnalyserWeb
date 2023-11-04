70b4c660785f29f0d4d3a44fbfaeaa83b7301ad8
Finn ONeill <56732347+Explorer017@users.noreply.github.com>
2023-11-04 21:25:51 +0000

    added the website

--- LogAnalyserSite/Data/FilterModel.cs
+++ LogAnalyserSite/Data/FilterModel.cs
@@ -2,5 +2,9 @@ namespace LogAnalyserSite.Data;
 
 public class FilterModel
 {
-    
+    public bool debugFilter;
+    public bool infoFilter;
+    public bool warnFilter;
+    public bool errorFilter;
+    public bool otherFilter;
 }
--- LogAnalyserSite/Data/LogLine.cs
+++ LogAnalyserSite/Data/LogLine.cs
@@ -2,5 +2,15 @@ namespace LogAnalyserSite.Data;
 
 public class LogLine
 {
-    
+    public string dateAndTime { get; private set; }
+    public string type { get; private set; }
+    public string message { get; private set; }
+
+    public LogLine(string dateAndTime, string type, string message )
+    {
+        this.dateAndTime = dateAndTime;
+        this.type = type;
+        this.message = message;
+    }
+
 }
