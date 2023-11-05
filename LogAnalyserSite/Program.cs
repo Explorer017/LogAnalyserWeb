6f6725ffb099bbee63363f5415f70a60375ee9e3
Jake Gillman <jakegillman3@gmail.com>
2023-11-05 09:10:05 +0000

    proper removed thing

--- LogAnalyserSite/Program.cs
+++ LogAnalyserSite/Program.cs
@@ -3,8 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddHttpClient();
-builder.Services.AddScoped<HttpClient>();
 
 var app = builder.Build();
 
